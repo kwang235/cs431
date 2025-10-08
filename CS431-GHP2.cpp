077bc15114ffcfb350e149dbcc0f75b468a5f7b0
Kevin <wangk@sunypoly.edu>
2025-10-08 09:39:59 -0400

    prototypes established for ghp 2

---
+++ CS431-GHP2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int n,a,j,k;
+
+void sort(void);
+void move(void);
+void findkay(void);
+
+int main(void){
+
+    return 0;
+}
+
+void sort(void){
+    return;
+}
+void move(void){
+    return;
+}
+void findkay(void){
+    return;
+}
