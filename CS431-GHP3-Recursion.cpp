955ca795499641a64f6bc87e7e8eb5fdd1c6f122
kevin <wangk@sunypoly.edu>
2025-11-08 10:04:12 -0500

    Commented driver and header files for GHP3

--- CS431-GHP3-Recursion.cpp
+++ CS431-GHP3-Recursion.cpp
@@ -1,20 +1,29 @@
 /*
+    Driver program to take user input of a sequence of characters along with a vector size
+    to pass into an instance of rsquare in order to create and display a concenctric square
+    of characters recursively
+
+    Written by Kevin W.
+    Language: C++ 
+    Compiler: MinGW G++
+    November 2025
 */
 
 #include <iostream>
 #include <vector>
-#include <rsquare.h>
+#include "rsquare.h"
 using namespace std;
 
 int main(void){
-    int k,temp;
+    int k;
+    char temp = ' ';
     vector<char> characters = {};
 
     cout<<"Please enter a size for the square "<<endl;
     cin>>k;
     cout<<"Enter a character to create the square with "<<endl;
-    for(int i = 0; i < k; i++){
-        cout<<"Character "<<i;
+    for(int i = 1; i <= k; i++){
+        cout<<"Character "<<i<<endl;
         cin>>temp;
         characters.push_back(temp);
     }
